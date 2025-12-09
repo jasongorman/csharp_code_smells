722fca59a073f368ae9b155e889b2ca5c8352ad3
Jason Gorman <jason.gorman@codemanship.com>
2025-12-09 11:42:18 +0000

    Added projects for Switch Statements

---
+++ SwitchStatements.Test/SorterTests.cs
@@ -0,0 +1,27 @@
+﻿namespace SwitchStatements.Test;
+
+public class SorterTests
+{
+    private static readonly int[] SortedArray = new[] { 1, 2, 3 };
+
+    [Test]
+    public void BubblesortPutsArrayInAscendingOrder() {
+        int[] array = {3,1,2};
+        array = new Sorter().Sort(SortKind.Bubble, array);
+        Assert.That(array, Is.EqualTo(SortedArray));
+    }
+	
+    [Test]
+    public void QuicksortPutsArrayInAscendingOrder() {
+        int[] array = {3,1,2};
+        array = new Sorter().Sort(SortKind.Quick, array);
+        Assert.That(array, Is.EqualTo(SortedArray));
+    }
+	
+    [Test]
+    public void InsertionsortPutsArrayInAscendingOrder() {
+        int[] array = {3,1,2};
+        array = new Sorter().Sort(SortKind.Insertion, array);
+        Assert.That(array, Is.EqualTo(SortedArray));
+    }
+}
