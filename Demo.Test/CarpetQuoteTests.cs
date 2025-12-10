43c7fa08963994df1fcb0ffa95520f440210c0cc
Jason Gorman <jason.gorman@codemanship.com>
2025-12-10 08:27:48 +0000

    Updated slide deck PDF

--- Demo.Test/CarpetQuoteTests.cs
+++ Demo.Test/CarpetQuoteTests.cs
@@ -2,5 +2,23 @@
 
 public class CarpetQuoteTests
 {
+    [Test]
+    public void QuoteForFittedCarpetWithNoAreaRounding()
+    {
+        CarpetQuote quote = new CarpetQuote();
+        
+        Assert.That(
+            quote.Calculate(5.25, 4.5, 10.0, false), 
+            Is.EqualTo(236.25));
+    }
     
+    [Test]
+    public void QuoteForFittedCarpetWithAreaRounding()
+    {
+        CarpetQuote quote = new CarpetQuote();
+        
+        Assert.That(
+            quote.Calculate(5.25, 4.5, 10.0, true), 
+            Is.EqualTo(240.0));
+    }
 }
--- Demo/CarpetQuote.cs
+++ Demo/CarpetQuote.cs
@@ -2,5 +2,15 @@
 
 public class CarpetQuote
 {
-    
+    public double Calculate(double length, double width, double pricePerSqMetre, bool roundUpArea)
+    {
+        double area = length * width;
+
+        if (roundUpArea)
+        {
+            area = (float)Math.Ceiling(area);
+        }
+        
+        return area * pricePerSqMetre;
+    }
 }
